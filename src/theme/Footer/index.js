7944c1970d9fb630bd586b05fcaa345d0c4e389e
ranjangautam <ranjan.gautam@aveshasystems.com>
2023-01-03 17:39:09 +0545

    fix(): copyright year changes to 2023

--- src/theme/Footer/index.js
+++ src/theme/Footer/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './footer.css'
 function Footer() {
   return (
-      <div className="row av-copyright-footer text-center">Copyright © Avesha 2022. All rights reserved.&nbsp; <a
+      <div className="row av-copyright-footer text-center">Copyright © Avesha 2023. All rights reserved.&nbsp; <a
           href="https://avesha.io/terms-and-condition/">Terms and Conditions</a>&nbsp;&nbsp;and&nbsp; <a
           href="https://avesha.io/privacy-policy/">Privacy Policy</a></div>
   );
