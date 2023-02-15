04b2a3f016fb408248ca76fbccb0d82b58b4e6a5
uma-kt <105046156+uma-kt@users.noreply.github.com>
2023-02-15 16:02:55 +0530

    AM-6715: OSS v0.6.0 release| Slice Gateway Updates | Release notes (#104)
    * create version-0.6.0 folder, update release notes, version-sidebar, and version folder
    
    * update the cross references from 0.4.0/0.5.0 to 0.6.0 version docs
    
    * update the cross references from 0.4.0/0.5.0 to 0.6.0 version docs
    
    * update the cross references in the version-0.5.0 folder
    
    * update overview section/ remove repetative paragraphs
    
    * fix typos
    
    * update kubeslice-cli docs
    
    * update kubeslice-cli docs
    
    * updated Calico URL
    
    * fix typos
    
    * update version in the docsaurus.config.js doc
    
    * revise the steps in the installkubeslice doc
    
    * revise the steps in the installkubeslice doc
    
    * update release date

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -40,7 +40,7 @@ const config = {
             'https://github.com/kubeslice/docs/tree/master/',
           routeBasePath: '/',
           includeCurrentVersion: false,
-          lastVersion: '0.5.0',
+          lastVersion: '0.6.0',
           versions:{
 
             /***
@@ -55,6 +55,11 @@ const config = {
              * }
              * 
              */
+            '0.6.0':{
+              label: '0.6.0',
+              path: '0.6.0',
+              banner: 'none'
+            },
             '0.5.0':{
               label: '0.5.0',
               path: '0.5.0',
